9a896fb17a1eb7f79bc26ee5b7c3f025b427d65e
shamik05 <64784024+shamik05@users.noreply.github.com>
2020-12-05 20:00:42 -0500

    Added nav and about me section

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        About
+      </li>
+      <li>
+        Projects
+      </li>
+      <li>
+        Skills
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "../Components/Nav";
+
+const Main = () => (
+  <>
+    <Nav />
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Modi, deserunt dicta totam dolore sunt quo
+      distinctio, esse quibusdam mollitia illo laudantium ratione delectus! Ipsa veniam
+      dolorem inventore ad optio molestiae.
+    </p>
+  </>
+);
+
+export default Main;
