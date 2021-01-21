8cb4e62cb0b5f27dc9de0484c256239cd0af71e5
shamik05 <shamik05@hotmail.com>
2021-01-21 16:57:10 -0500

    Added all group projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { Container, CardGroup, Card } from "react-bootstrap";
+import {
+  Container, CardDeck, Card, Badge,
+} from "react-bootstrap";
 import repos from "../helpers/constants";
 
 const Projects = () => (
   <Container>
-    <CardGroup>
+    <CardDeck>
       {repos.map((e) => (
         <Card key={e.id}>
           <Card.Header>{e.name}</Card.Header>
           {/* <Card.Img variant="top" src={e.img} /> */}
+
           <Card.Body>
             <Card.Text>{e.description}</Card.Text>
           </Card.Body>
-          <Card.Footer>{e.tech}</Card.Footer>
+
+          <Card.Footer>
+            {e.tech.map((t) => (
+              <Badge pill variant="light">{t}</Badge>
+            ))}
+          </Card.Footer>
         </Card>
       ))}
-    </CardGroup>
+    </CardDeck>
   </Container>
 );
 
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -2,11 +2,26 @@ const repos = [
   {
     name: "Balls of War",
     id: "bow",
-    description: "MERN Stack virtual NBA-themed card game based on War! ",
+    description: "MERN Stack virtual NBA-themed card game based on War!",
     storyLong: "Due to the ongoing pandemic, the NBA season have been interrupted and social gatherings restricted which meant no basketball for months. Seeing as current basketball games on the market are expensive and also take time to develop, we wanted to create a simple, but fun interactive basketball related game that could be played with a friend. The inspiration for the game came from the childhood favourite card game WAR.",
     storyShort: "",
-    tech: ["mongo", "express", "react", "node", "mysql", "reactrouter", "sass", "bootstrap", "passport"],
-    img: "https://raw.githubusercontent.com/liam-mack/Balls-of-War/main/images/Deployed.png",
+    tech: ["Mongo", "Express", "React", "Node", "MySql", "React-Router", "Sass", "Bootstrap", "Passport"],
+  },
+  {
+    name: "Wheels Gone Wild",
+    id: "wgw",
+    description: "Your one stop for everything bike. An e-commerce website with user account and order functionality!",
+    storyLong: "An e-commerce website built using handlebars and a MySQL database of 40 bikes. The website comes with an user and admin interface authenticated through passport. Bikes can be filtered 7 different ways and can be added to a shopping cart to a checkout.",
+    storyShort: "",
+    tech: ["Express", "Handlebars", "Fileupload", "Node", "MySql", "Sequelize", "Passport"],
+  },
+  {
+    name: "LSD",
+    id: "lsd",
+    description: "An exchange-neutral tool designed for the modern cryptocurrency traders",
+    storyLong: "An e-commerce website built using handlebars and a MySQL database of 40 bikes. The website comes with an user and admin interface authenticated through passport. Bikes can be filtered 7 different ways and can be added to a shopping cart to a checkout.",
+    storyShort: "",
+    tech: ["HTML", "CSS3", "APIs", "jQUery", "Chartjs", "Momentjs", "Materialize", "Material Design"],
   },
 ];
 
