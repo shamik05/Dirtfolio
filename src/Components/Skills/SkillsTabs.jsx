5e4ac005c6ecec1069f2b043e1166a121fe6d53b
shamik05 <shamik05@hotmail.com>
2021-03-14 21:44:51 -0400

    Pruned skill cards css

--- src/Components/Skills/SkillsTabs.jsx
+++ src/Components/Skills/SkillsTabs.jsx
@@ -1,16 +1,23 @@
+/* eslint-disable */
 import React from "react";
 import {
-  Tab, TabList, Tabs,
+  Tab, TabList, Tabs, TabPanel,
 } from "react-tabs";
+import SkillCard from "./SkillCard";
+import { Languages } from "../../Constants/Skillset";
 import "./SkillsTabs.scss";
 
 const SkillsTabs = ({ tabs }) => (
   <Tabs className="skills__tabs__wrapper">
-    <TabList className="skills__tabs">
+    <TabList className="skills__tablist">
       {tabs.map((tab) => (
-        <Tab key={tab}>{tab}</Tab>
+        <Tab key={tab}><h3>{tab}</h3></Tab>
       ))}
     </TabList>
+
+    <TabPanel>
+      <SkillCard list={Languages} />
+    </TabPanel>
   </Tabs>
 );
 
--- src/Views/Skills.jsx
+++ src/Views/Skills.jsx
@@ -1,10 +1,6 @@
 /* eslint-disable */
 import React from "react";
-import ServiceList from "../Components/SkillCard";
 import SkillsTabs from "../Components/Skills/SkillsTabs";
-import {
-  Languages, Frameworks, Libraries, Others,
-} from "../Constants/Skillset";
 import "./Skills.scss";
 
 const tabs = ["Languages", "Frameworks", "Libraries", "Others"];
@@ -15,34 +11,10 @@ const Skills = () => (
       <div className="container">
         <h2 className="skills__title text-center">Skills</h2>
 
-        <div className="row">
-          <div className="col-lg-12">
+        <div className="row no-gutters">
+          {/* <div className="col-lg-12"> */}
             <SkillsTabs tabs={tabs} />
-            {/* <Tabs className="skills__tabs__wrapper">
-              <TabList className="skills__tabs">
-                <Tab>Languages</Tab>
-                <Tab>Frameworks</Tab>
-                <Tab>Libraries</Tab>
-                <Tab>Others</Tab>
-              </TabList>
-              <TabPanel>
-                <ServiceList column="col-lg-4 col-md-6 col-sm-6 col-12 text-left" list={Languages} />
-              </TabPanel>
-
-              <TabPanel>
-                <ServiceList column="col-lg-4 col-md-6 col-sm-6 col-12 text-left" list={Frameworks} />
-              </TabPanel>
-
-              <TabPanel>
-                <ServiceList column="col-lg-4 col-md-6 col-sm-6 col-12 text-left" list={Libraries} />
-              </TabPanel>
-
-              <TabPanel>
-                <ServiceList column="col-lg-4 col-md-6 col-sm-6 col-12 text-left" list={Others} />
-              </TabPanel>
-
-            </Tabs> */}
-          </div>
+          {/* </div> */}
         </div>
       </div>
     {/* </div> */}
