5b90752a545b30031c926ea6f18846c894132b55
Shamik H <shamik05@hotmail.com>
2022-12-20 18:51:48 -0500

    Rebuild portfolio

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,6 @@
-const reportWebVitals = (onPerfEntry) => {
+const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({
-      getCLS, getFID, getFCP, getLCP, getTTFB,
-    }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -13,17 +11,3 @@ const reportWebVitals = (onPerfEntry) => {
 };
 
 export default reportWebVitals;
-
-// "@testing-library/jest-dom": "^5.11.6",
-// "@testing-library/react": "^11.2.5",
-// "@testing-library/user-event": "^12.6.3",
-
-// "@emotion/react": "^11.1.5",
-//     "@emotion/styled": "^11.1.5",
-//     "@material-ui/core": "^5.0.0-alpha.25",
-//     "@material-ui/icons": "^4.11.2",
-//     "@material-ui/system": "^4.11.3",
-// "bootstrap": "^5.0.0-beta2",
-
-Libraries: React, React-Router, jQuery, Mongoose.js, Socket.IO, Passport.js, Inquirer.js, Moment.js, Chart.js
-Others: MySQL, MongoDB, Sequelize, AWS, Git version control, GitHub, Heroku, ESLint, Dynamics CRM
